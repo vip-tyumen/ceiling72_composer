04dae445b0762092e6a748efb193922166c652be
ProjectSoft-STUDIONIONS <projectsoft2009@yandex.ru>
2022-09-03 15:46:24 +0400

    update

--- src/PrepareDLMenu.php
+++ src/PrepareDLMenu.php
@@ -5,12 +5,15 @@ class PrepareDLMenu {
 
 	public static function prepareHome(array $data, \DocumentParser $modx, $_DL, \prepare_DL_Extender $_extDocLister)
 	{
+		$func = function($a) {
+			return (int)$a;
+		};
 		$sheme = $_DL->getCFGDef('urlScheme');
 		$site = $modx->config['site_name'];
-		$ignored = $modx->config['ignore_menu_ids'] || '';
-		$ignored = explode(',', $ignored);
-		if(array_search($data['id'], $ignored)):
-			$data['url'] = $modx->makeUrl($modx->documentIdentifier, '', '', $sheme);
+		$ignored = $modx->config['ignore_menu_ids'] ? $modx->config['ignore_menu_ids'] : '';
+		$ignored = array_map($func, explode(',', $ignored));
+		if(in_array((int)$data['id'], $ignored, true)):
+			$data['url'] = $modx->makeUrl($modx->config['site_start'], '', '', $sheme) . "#" . $data["alias"];
 		endif;
 		$data['id'];
 		return $data;
